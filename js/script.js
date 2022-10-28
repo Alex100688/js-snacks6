760bf1d31ed62b448104e914f3fcabec3ee1e9e9
Alex100688 <alessiozingaro2@gmail.com>
2022-10-28 10:41:36 +0200

    eesercizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const number=Number[`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`];
+console.log(number);
+let i=0;
+let risultato= (i % 2)===false;
+while (i< number && risultato===false) {
+    if (i<number && risultato==true) {
+     i++ 
+    }
+    console.log(number);
+}
